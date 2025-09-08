a61347a75ccb060df2574325d6d551c51d1e20ed
rattphumi <darkdamonig@gmail.com>
2025-09-08 11:48:31 +0700

    aa

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -10,16 +10,16 @@
 //   onDelete: () => void;
 // };
 
-export default function ItemCard() {
-  // หากต้องการเปลี่ยนแปลง type ชนิด string เป็น number สามารถใช้วิธีการดังโค้ดตัวอย่างด้านล่างนี้ได้
-  let val_number: number = Number("500.0");
-  console.log(val_number + 100); // 600.0
-  // const [foods, setFoods] = useState<FoodProps[]>([]);
+// export default function ItemCard() {
+//   // หากต้องการเปลี่ยนแปลง type ชนิด string เป็น number สามารถใช้วิธีการดังโค้ดตัวอย่างด้านล่างนี้ได้
+//   let val_number: number = Number("500.0");
+//   console.log(val_number + 100); // 600.0
+//   // const [foods, setFoods] = useState<FoodProps[]>([]);
   
 
-  return {
-    /* Type additional text here. */
+//   return {
+//     /* Type additional text here. */
     
     
-  };
-}
+//   };
+// }
